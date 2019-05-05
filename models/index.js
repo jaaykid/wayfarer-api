b84ff37b541d5c25762977dedddec99a469b65c7
Justin Curry <justincurrydev@gmail.com>
2019-05-05 12:40:14 -0700

    updated server

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL = 'mongodb://localhost:27017/wayfarer'
+const DB_URL = 'mongodb://https://blogging-tings.herokuapp.com/'
 
 mongoose.connect(DB_URL, {useNewUrlParser: true, useFindAndModify: false})
 .then(() => console.log('mongodb is connected...'))
