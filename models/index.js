92e04af8fcba841148d310953e8a56309668f663
Justin Curry <justincurrydev@gmail.com>
2019-05-05 13:59:32 -0700

    removed bad files

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 const DB_URL = 'mongodb://localhost:27017/wayfarer'
-const MONGOLAB_CRIMSON_URI = 'mongodb://heroku_kmf2q9jc:r0p6kd6ebl12vegpa7vj3jc55f@ds229609.mlab.com:29609/heroku_kmf2q9jc'
 
-mongoose.connect(process.env.MONGOLAB_CRIMSON_URI || DB_URL, {useNewUrlParser: true, useFindAndModify: false})
+mongoose.connect(process.env.MONGODB_URI || DB_URL , {useNewUrlParser: true, useFindAndModify: false})
 .then(() => console.log('mongodb is connected...'))
 .catch((err) => console.log(err))
 
