83d798ecd50dce48cbe27aad4f299297e8b24800
Justin Curry <justincurrydev@gmail.com>
2019-05-05 12:10:00 -0700

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
 // Controllers 
 const profileCtrl = require('./controllers/profileCtrl');
